4821fc8a26eb0d70638beb8781c696b93a7eaf94
Sashi Bhumaraju <sasi.bhumaraju@gmail.com>
2023-09-28 12:40:52 +0530

    new media links added

--- src/components/MediaPlayList.js
+++ src/components/MediaPlayList.js
@@ -10,7 +10,8 @@ class MediaPlayList extends React.Component {
         return(
         <div className='MediaPlayList'>
               
-              <MediaPlayer VideoLink={'https://youtu.be/CE98BLolEhM'} MediaIconLink={yIcon} MediaDescrition={'Walk through'}></MediaPlayer>
+              <MediaPlayer VideoLink={'https://www.youtube.com/watch?v=TEqDqqcCapI'} MediaIconLink={yIcon} MediaDescrition={'Walk through'}></MediaPlayer>
+              <MediaPlayer VideoLink={'https://www.youtube.com/watch?v=qHEZiIGwokk'} MediaIconLink={yIcon} MediaDescrition={'Location video'}></MediaPlayer>
               <MediaPlayer VideoLink={'https://www.youtube.com/watch?v=0MVwRYFJroo'} MediaIconLink={yIcon} MediaDescrition={'Digital site visit'}></MediaPlayer>
               <MediaPlayer Image360Link={'https://app.lapentor.com/sphere/silpa-raghava-botanica'} MediaIconLink={camera} MediaDescrition={'Arial 360 view'}></MediaPlayer>
               {/* <MediaPlayer VideoLink={'https://www.youtube.com/watch?v=ysz5S6PUM-U'} MediaIconLink={yIcon} MediaDescrition={'Site Map'}></MediaPlayer> */}
